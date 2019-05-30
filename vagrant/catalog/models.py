fe2be9506f8fff13d7bec076f76e300adf996888
tan <haitanle88@gmail.com>
2019-05-30 10:58:46 -0400

    add pep8 style to models.py

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -5,52 +5,53 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
-	__tablename__ = 'user'
+    __tablename__ = 'user'
 
-	id = Column(Integer, primary_key=True)
-	username = Column(String, nullable=False)
-	password = Column(String, nullable=True) #password not needed - log in with only OAuth
-	email = Column(String)
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    # password not needed - log in with only OAuth
+    password = Column(String, nullable=True)
+    email = Column(String)
 
-	teams = relationship("Team")
-	players = relationship("Player")
+    teams = relationship("Team")
+    players = relationship("Player")
 
 
 class Team(Base):
-	__tablename__ = 'team'
+    __tablename__ = 'team'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String, nullable=False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	players = relationship("Player")
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    players = relationship("Player")
 
-	@property
-	def serialize(self):
-		return {
-			'teamName': self.name
-		}
-	
+    @property
+    def serialize(self):
+        return {
+            'teamName': self.name
+        }
+
 
 class Player(Base):
-	__tablename__ = 'player'
+    __tablename__ = 'player'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String, nullable=False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	bio = Column(String, nullable= True)
-	position = Column(String)
-	team_id = Column(Integer, ForeignKey('team.id'))
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    bio = Column(String, nullable=True)
+    position = Column(String)
+    team_id = Column(Integer, ForeignKey('team.id'))
 
-	@property
-	def serialize(self):
-		return {
-			'playerName': self.name,
-			'position': self.position,
-			'bio': self.bio
-		}
+    @property
+    def serialize(self):
+        return {
+            'playerName': self.name,
+            'position': self.position,
+            'bio': self.bio
+        }
 
 
 engine = create_engine('sqlite:///soccerCatalog.db')
 Base.metadata.create_all(engine)
-
